987258a5f4f6125f7386c6b1d96d1181d7995323
pavan-36 <90022373+pavan-36@users.noreply.github.com>
2021-11-01 20:58:13 +0530

    Create second.js

---
+++ second.js
@@ -0,0 +1,6 @@
+var toys = "6G";
+console.log(" the number of toys are " + toys );
+var b = function(){
+  var toys = "3L";
+  console.log(" the number of toys are " + toys ); }
+console.log(" the number of toys are " + toys );
