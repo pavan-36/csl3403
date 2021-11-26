52d8047145a9f400003c3e5d203e6d5e56baf91a
pavan-36 <90022373+pavan-36@users.noreply.github.com>
2021-11-26 20:20:43 +0530

    Update second2.js

--- second2.js
+++ second2.js
@@ -8,6 +8,7 @@ var person1 = new person ("Arjun", "Vandanapu", "Tirumala");
 var person2 = new person ("Pavan", "Maddula", "Tenali");
 var person3 = new person ("Vaishnavi", "Manepalli", "Kancheepuram");
 
-console.log(person1.fname);
-console.log(person2.fname);
-console.log(person3.fname);
+console.log("The detials of First person are as follows \n " + "first name : " + person1.fname + "last name : " + person1.lname + "office address : " + person1.offadd);
+console.log("The detials of Second person are as follows \n " + "first name : " + person2.fname + "last name : " + person2.lname + "office address : " + person2.offadd);
+console.log("The detials of Third person are as follows \n " + "first name : " + person3.fname + "last name : " + person3.lname + "office address : " + person3.offadd);
+
